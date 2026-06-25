80dfd8c7a226971a4989689ff7157f76a3680af5
tom19780617 <tom19780617@gmail.com>
2026-06-25 16:49:35 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: 'https://tom19780617.github.io' }));
 app.use(express.static('public'));
 
 const db = mysql.createConnection({
